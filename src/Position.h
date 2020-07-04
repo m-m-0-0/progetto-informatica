858cbbe0aa268ffbc7e143d7c15119e76b2e44e0
mm00 <molarimauro00@gmail.com>
2020-07-04 22:43:16 +0200

    removed unnecessary include guard

--- src/Position.h
+++ src/Position.h
@@ -1,11 +1,6 @@
-//
-// Created by mm00 on 14-May-20.
-//
-
 #ifndef POSITION_OBJECT_H
 #define POSITION_OBJECT_H
 
-#endif //POSITION_OBJECT_H
 
 class Position{
 public:
@@ -20,7 +15,4 @@ public:
     bool operator==(const Position& a) const;
 };
 
-#ifndef RACING_POSITION_H
-#define RACING_POSITION_H
-
-#endif //RACING_POSITION_H
+#endif //POSITION_OBJECT_H
