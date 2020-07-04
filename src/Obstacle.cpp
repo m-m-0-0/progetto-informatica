2f93e5d871991c5b95891a5945532b3e833ffa93
mm00 <molarimauro00@gmail.com>
2020-07-04 22:43:45 +0200

    added character representation for Obstacle

--- src/Obstacle.cpp
+++ src/Obstacle.cpp
@@ -1,14 +1,16 @@
 #include "Obstacle.h"
 
 Obstacle::Obstacle(int x, int y, int points){
+    this->type = Type::Obstacle;
     this->points = points;
     this->pos = Position(x, y);
+    this->character = 'X';
 }
 
 void Obstacle::setPoints(int p) {
     points = p;
 }
 
- int Obstacle::getPoints(){
+int Obstacle::getPoints(){
     return points;
 }
