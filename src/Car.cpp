14c5c9a28f70697482bc699ebd1a7538783b5d57
mm00 <molarimauro00@gmail.com>
2020-07-04 22:40:45 +0200

    added constructor, misc. fixes

--- src/Car.cpp
+++ src/Car.cpp
@@ -3,12 +3,20 @@
 Car::Car(){
     this->speed = 0;
     this->pos = Position(0,0);
-    this->type = Object::Type::Car;
+    this->type = Type::Car;
+    this->character = 'A';
 }
 
-Car::Car(Type type, int x, int y, int points, int speed) {
+Car::Car(int x, int y, int points) {
+    this->speed = 1;
+    this->type = Type::Car;
+    this->pos = Position(x,y);
+    this->character = 'A';
+}
+
+Car::Car(int x, int y, int points, int speed) {
     this->speed = speed;
-    this->type = Object::Type::Car;
+    this->type = Type::Car;
     this->pos = Position(x,y);
     this->character = 'A';
 }
--- src/Car.h
+++ src/Car.h
@@ -1,13 +1,13 @@
 #ifndef RACING_CAR_H
 #define RACING_CAR_H
 
-
 #include "Object.h"
 
 class Car: Object{
 public:
     Car();
-    Car(Type, int, int, int, int);
+    Car(int, int, int, int);
+    Car(int, int, int);
     int getSpeed();
     void setSpeed(int);
     void move(Position dir);
